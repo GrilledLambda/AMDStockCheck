a3aecb89ace6e195872f7786e8954a0c8d10407a
Peter <76958651+Peterjjones98@users.noreply.github.com>
2021-04-09 14:03:45 -0700

    Add files via upload

---
+++ AmdChecker.py
@@ -0,0 +1,83 @@
+import discord, time , requests
+from discord.ext import commands
+from discord.ext.commands import Bot
+from discord.utils import get
+from bs4 import BeautifulSoup
+import lxml
+from BotConfigurations import TOKEN, GUILD, linkFile, refreshDelay, debug
+#----------------------------
+
+client = discord.Client() 
+
+#-------------------------MAIN BOT CLASS--------------------------
+class CheckStock:
+
+    #checks if product link is in stock
+    def checkAval(self, url, title=False):
+        
+        headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0", "Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.content, features="lxml")
+       
+        try:
+            stock = soup.select("p.product-out-of-stock")[-1].get_text()
+        except:
+            stock = "Couldn't determine availability."
+        
+        if title:
+            try:
+                title = soup.select(".product-page-description >  h2")[-1].get_text()
+            except:
+                title = "Couldn't get product title"
+            result = [title, stock]
+            return result
+        else:
+            return stock
+
+    #checks stock of link. If in stock it sends a discord message.
+    def checkMe(self,url):
+    
+        embed=discord.Embed(title="**__IN STOCK ALERT__**", color=0xff0000) #an alerting red >:[
+        stock = self.checkAval(url, True)
+        if debug:
+            print("stock:", stock)
+        elif "Out of stock" not in stock[1]:
+            embed.add_field(name=stock[0], value=f"**{stock[1]}**", inline=False)
+            embed.add_field(name="Source:", value=f"**{url}**", inline=False)
+            return  embed
+        else:
+            return False    
+
+    async def runAll(self):
+        while True:
+            status = self.checkAll()
+            if status:
+                channel = client.get_channel(GUILD)
+                await channel.send("@everyone", embed=status)
+
+newBot = CheckStock()
+
+#loops through .txt file and checks stock
+def startBot():
+    while True:
+        with open(linkFile) as linkReader:
+                for link in linkReader:
+                    if debug:
+                        print("current link:", link, "\n Checking Link...")
+                    time.sleep(refreshDelay)
+                    status = newBot.checkMe(link.replace("\n", ""))
+                    if status == True:
+                        channel = client.get_channel(GUILD)
+                        channel.send("@everyone", embed=status)
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+    print(f"{client.user} successfully connected to: {guild.name}")
+    return startBot()
+
+if __name__ == "__main__":
+    client.run(TOKEN)
+    
---
+++ BotConfigurations.py
@@ -0,0 +1,61 @@
+
+###-------------START HERE--------------------###
+
+"""
+
+    1. download Python : https://www.python.org/ftp/python/3.9.4/python-3.9.4-amd64.exe
+    make sure when you're installing it you select 'Add to .PATH' in the installation Wizzard.
+
+    2. Run CMD and enter these commands one after the other:
+                pip install -U discord.py
+                pip install requests
+                pip install lxml
+                pip install bs4
+
+    3. Create a discord channel you want you bot to hangout in.
+
+    4. Create an account at https://discord.com/developers
+
+    5. Create a New Application (Bot)
+
+    6. Once you've selected your Application go to: OAuth2 > Scopes **Select bot** > Bot permissions **Select Administrator**
+
+    7. Under Scopes you should see a new link.
+    Going to that link will take you to discord where you can select which channel you authorize your bot to opperate in.
+
+    8. After these steps yor bot is ready to go. Now fill out the intializers below.
+
+"""
+
+#Initializers------>>
+
+#---------------------TIME DELAY------------------------------------
+
+#This is the time (in seconds) the script will wait before sending an HTTP request to AMD's servers.
+#Less time means the bot will be faster but AMD might flag your IP.
+refreshDelay = .5 
+
+#-------------------------TOKEN-------------------------------------
+
+#This is the Authorization Token of your DiscordBot. This is found under your Discord Developer Portal > Select your Bot > Bot > Token
+TOKEN = ""
+
+#-------------------------GUILD--------------------------------------
+
+#This is the ID of the discord channel you want the bot to message when it finds an item in stock.
+#You can find this by going into the discord channel and right clicking the channel-name in the top left of the screen.
+#There will be an option to 'Copy ID'
+GUILD = ""
+
+#-------------------------FILE LOCATION------------------------------
+
+#this is the path of your links.txt file with the AMD links.
+#Leave this default if the file is in the same dir as the script.
+linkFile = "links.txt"
+
+#------------------------OPTIONAL------------------------------------
+
+#This will run the script as normal but it will print off the stock of each item.
+#run this to make sure the bot is gathering information correctly and to see custom error messages.
+
+debug = True
